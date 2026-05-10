ddc0565a00b40720c5b07b74cb3f2b6f1484be0b
Yashwant Kumar <153898847+HappyKn1ght@users.noreply.github.com>
2026-05-10 08:49:18 +0530

    prime checker

---
+++ C++/OOPs/primeChecker.cpp
@@ -0,0 +1,38 @@
+// program to check prime number usng class and object.
+#include <iostream>
+using namespace std;
+
+class PrimeCheck
+{
+public:
+    void isPrime(int num)
+    {
+        bool prime = true;
+        if (num <= 1)
+            prime = false;
+        else
+        {
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        if (prime)
+            cout << num << " is a Prime Number.";
+        else
+            cout << num << " is not a Prime Number.";
+    }
+};
+int main()
+{
+    PrimeCheck obj;
+    int num;
+    cout << "Enter the Number: ";
+    cin >> num;
+    obj.isPrime(num);
+    return 0;
+}

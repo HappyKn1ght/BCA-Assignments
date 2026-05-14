94c942abcc893b27c7a9e1f27821bb61f1a7514f
Yashwant Kumar <153898847+HappyKn1ght@users.noreply.github.com>
2026-05-14 10:15:39 +0530

    comments

---
+++ Java/Comments.java
@@ -0,0 +1,9 @@
+public class Comments{
+    public static void main(String[] args){
+        // This is Single-line comments
+        /*
+        This is Multi-line comments
+         */
+        System.out.println("This program shows Single and Multi-line comments in Java.");
+    }
+}

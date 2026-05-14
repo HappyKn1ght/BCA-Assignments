0884510c6c9a31fdbd44ba6e92f02eab2a4caca6
Yashwant Kumar <153898847+HappyKn1ght@users.noreply.github.com>
2026-05-14 10:15:19 +0530

    Add

---
+++ Java/Add.java
@@ -0,0 +1,11 @@
+//Quick quiz - Write program to add three numbers.
+public class Add {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        int sum = a + b + c;
+        System.out.println("Sum: " + sum);
+      //System.out.println(sum);
+    }
+}

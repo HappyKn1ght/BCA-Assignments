9d5e9ef7223f71cbc2531601513e22b755b39ec0
Yashwant Kumar <153898847+HappyKn1ght@users.noreply.github.com>
2026-05-14 10:14:10 +0530

    Add files via upload

---
+++ Java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        String name = "Happy";
+        System.out.println("Hello World!\n");
+        System.out.println("I'm new to Java and learning");
+        System.out.println("My name is " + name);
+        System.out.println("and I'm "+age);
+    }
+}

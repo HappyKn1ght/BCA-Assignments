0c9c1e3cacf2fb1af28b7471fdf41a2a481b90ab
Yashwant Kumar <153898847+HappyKn1ght@users.noreply.github.com>
2026-05-21 08:03:02 +0530

    Pattern_Printing

---
+++ Java/Pattern_Printing.java
@@ -0,0 +1,13 @@
+public class Pattern_Printing {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            for(int j = 0; j<i;j++){
+
+                System.out.print("* ");
+                // System.out.println(i);
+            }
+            System.out.println();
+        }
+    }
+
+}
